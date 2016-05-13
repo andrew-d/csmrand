d6b03b947ce65d22e4c5566dbbcea3d76f0b3280
Andrew Dunham <adunham@stripe.com>
2016-05-13 15:20:34 -0700

    Document panicking behaviour

--- doc.go
+++ doc.go
@@ -2,4 +2,11 @@
 // by the CSPRNG in crypto/rand.  This allows using the nicer API from
 // math/rand, but still ensuring that the random number source is
 // cryptographically-secure.
+//
+// If there is a case where this package cannot obtain random data from
+// crypto/rand, it will panic.  This is largely the solution you want, since
+// this should only occur (on Linux) when the /dev/urandom pool hasn't been
+// initialized yet.  In this case, the calling code can't do anything sensible
+// with the error anyway.  Also, panicking on this error allows us to maintain
+// the same API as math/rand.
 package csmrand
