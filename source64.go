d78c565c61ca90919f7d388326fedbfafc3f3816
Andrew Dunham <andrew@du.nham.ca>
2019-08-21 23:32:10 -0700

    Implement math/rand.Source64 as well

---
+++ source64.go
@@ -0,0 +1,24 @@
+// +build go1.8
+
+package csmrand
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	mrand "math/rand"
+)
+
+// Implement the math/rand.Source64 interface.  Will panic if an error occurs
+// when reading the underlying Reader.
+func (r CSRandSource) Uint64() uint64 {
+	var i uint64
+
+	err := binary.Read(rand.Reader, binary.LittleEndian, &i)
+	if err != nil {
+		panic("could not read random data")
+	}
+
+	return i
+}
+
+var _ mrand.Source64 = CSRandSource{}
